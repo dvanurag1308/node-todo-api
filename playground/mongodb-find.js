0dcba95d15a82f42a293ee3424b66776b3028eda
dvanurag1308 <dvanurag1308@gmail.com>
2017-07-20 21:29:45 -0400

    initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB Server');
+
+    // db.collection('Todos').find({
+    //     _id : new ObjectID('597150176844e1adb3883a74')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to get count', err);
+    // });
+
+    db.collection('Users').find({name: 'Anurag'}).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+
+
+    //db.close();
+});

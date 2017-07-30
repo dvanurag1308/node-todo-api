c14a47d968add232d2bb52c2731c0df728805765
dvanurag1308 <dvanurag1308@gmail.com>
2017-07-30 18:48:13 -0400

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
+mongoose.connect(process.env.PORT?'mongodb://anurag:chicken65@ds129023.mlab.com:29023/node-todoapp':'mongodb://localhost:27017/TodoApp', {
     useMongoClient: true
 });
 

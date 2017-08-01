dbd552241ac26f15ea43d5d0c5cc28578b9a1eff
dvanurag1308 <dvanurag1308@gmail.com>
2017-07-31 20:07:09 -0400

    config file port name

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ if(env === 'development'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
 } else if(env === 'test'){
-    process.env.port = 3000;
+    process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
 } else{
     process.env.MONGODB_URI = 'mongodb://anurag:chicken65@ds129023.mlab.com:29023/node-todoapp'

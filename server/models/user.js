4f054c0a751a2451ce8a41d38e56d20590a46d24
dvanurag1308 <dvanurag1308@gmail.com>
2017-08-05 18:31:49 -0400

    adding authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -48,6 +48,23 @@ UserSchema.methods.generateAuthToken = function() {
 
     return user.save().then(() => {
         return token;
+    })
+};
+
+UserSchema.statics.findByToken = function(token) {
+    var User = this;
+    var decoded;
+
+    try{
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e){
+        return Promise.reject();
+    }
+
+    return User.find({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth' 
     });
 };
 

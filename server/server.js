f5563e1fc8045d5bdd3d54f0f751a056db84a811
dvanurag1308 <dvanurag1308@gmail.com>
2017-08-06 20:56:32 -0400

    POST /users/login route

--- server/server.js
+++ server/server.js
@@ -107,7 +107,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me',authenticate, (req, res) => {
     res.send(req.user);
-})
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(body.email, body.password).then((user) => {
+      return user.generateAuthToken().then((token) => {
+          res.header('x-auth', token).send(user);
+        });
+    }).catch((err) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server Started on port: ${port}`);

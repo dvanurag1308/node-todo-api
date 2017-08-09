6693a89540a303308f2cc6108d4fe10d905ee497
dvanurag1308 <dvanurag1308@gmail.com>
2017-08-08 22:15:06 -0400

    remove commented code

--- server/config/config.js
+++ server/config/config.js
@@ -8,13 +8,3 @@ if(env === 'development' || env === 'test'){
         process.env[key] = envConfig[key];
     });
 }
-
-// if(env === 'development'){
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-// } else if(env === 'test'){
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
-// } else{
-//     process.env.MONGODB_URI = 'mongodb://anurag:chicken65@ds129023.mlab.com:29023/node-todoapp'
-// }
